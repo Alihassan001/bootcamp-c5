051325caddba9bf8e5beec2845200016d1a293cf
Alihassan001 <ali.official001@gmail.com>
2020-06-09 22:51:17 +0500

    adding room state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css'
 
 function App() {
   const [lit, setLit] = useState(false)
+  const [room, setRoom] = useState(0)
   return (
     <div className='App'>
       <h1>React States - class 4 </h1>
       <p>The room is {lit ? 'lit' : 'dark'}</p>
       <button onClick={ () => setLit(!lit) }>
         Turn {lit ? 'OFF' : 'ON'} lit</button>
+
+      <p>The available rooms are: {room}</p>
+      <button onClick={ () => setRoom(room + 1) }>Add Room</button>
     </div>
   );
 }
